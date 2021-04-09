9640c72c42b0bc2636dd936a54bfaeecec6f10e0
shaokai <shaokaiyeah@gmail.com>
2021-04-09 14:22:14 +0000

    add more

---
+++ configs/bottom_up/resnet/fish/res50_fish_512x512.py
@@ -0,0 +1,171 @@
+log_level = 'INFO'
+#load_from = 'checkpoints/res50_coco_512x512-5521bead_20200816.pth'
+#load_from = 'checkpoints/resnet50_swav.pth'
+load_from = None
+resume_from = None
+dist_params = dict(backend='nccl')
+workflow = [('train', 1)]
+checkpoint_config = dict(interval=100)
+evaluation = dict(interval=10, metric='mAP', key_indicator='AP')
+
+sigma = 2
+max_num_people = 30
+
+optimizer = dict(
+    type='Adam',
+    lr=0.00015,
+)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=10,
+    warmup_ratio=0.001,
+    step=[1000, 2000])
+total_epochs = 3000
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+
+channel_cfg = dict(
+    dataset_joints=10,
+    dataset_channel=range(10),
+    inference_channel=range(10)
+)
+
+data_cfg = dict(
+    image_size=512,
+    base_size=256,
+    base_sigma=sigma,
+    heatmap_size=[128],
+    num_joints=channel_cfg['dataset_joints'],
+    dataset_channel=channel_cfg['dataset_channel'],
+    inference_channel=channel_cfg['inference_channel'],
+    num_scales=1,
+    scale_aware_sigma=False,
+)
+
+# model settings
+model = dict(
+    type='BottomUp',
+    pretrained='torchvision://resnet50',
+    backbone=dict(type='ResNet', depth=50),
+    keypoint_head=dict(
+        type='BottomUpSimpleHead',
+        in_channels=2048,
+        num_joints=10,
+        tag_per_joint=True,
+        with_ae_loss=[True],
+        loss_keypoint=dict(
+            type='MultiLossFactory',
+            num_joints=10,
+            num_stages=1,
+            ae_loss_type='exp',
+            with_ae_loss=[True],
+            push_loss_factor=[0.001],
+            pull_loss_factor=[0.001],
+            with_heatmaps_loss=[True],
+            heatmaps_loss_factor=[1.0],
+        )),
+    train_cfg=dict(
+        num_joints=channel_cfg['dataset_joints'],
+        img_size=data_cfg['image_size']),
+    test_cfg=dict(
+        num_joints=channel_cfg['dataset_joints'],
+        max_num_people=max_num_people,
+        scale_factor=[1],
+        with_heatmaps=[True],
+        with_ae=[True],
+        project2image=True,
+        nms_kernel=5,
+        nms_padding=2,
+        tag_per_joint=True,
+        detection_threshold=1e-1,
+        tag_threshold=1,
+        use_detection_val=True,
+        ignore_too_much=False,
+        adjust=True,
+        refine=True,
+        flip_test=False))
+
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='BottomUpRandomAffine',
+        rot_factor=30,
+        scale_factor=[0.75, 1.5],
+        scale_type='short',
+        trans_factor=40),
+    dict(type='BottomUpRandomFlip', flip_prob=0.5),
+    dict(type='ToTensor'),
+    dict(
+        type='NormalizeTensor',
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]),
+    dict(
+        type='BottomUpGenerateTarget',
+        sigma=sigma,
+        max_num_people=max_num_people,
+    ),
+    dict(
+        type='Collect',
+        keys=['img', 'joints', 'targets', 'masks'],
+        meta_keys=[]),
+]
+
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='BottomUpGetImgSize', test_scale_factor=[1]),
+    dict(
+        type='BottomUpResizeAlign',
+        transforms=[
+            dict(type='ToTensor'),
+            dict(
+                type='NormalizeTensor',
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]),
+        ]),
+    dict(
+        type='Collect',
+        keys=['img'],
+        meta_keys=[
+            'image_file', 'aug_data', 'test_scale_factor', 'base_size',
+            'center', 'scale', 'flip_index'
+        ]),
+]
+
+test_pipeline = val_pipeline
+
+data_root = 'data/fish'
+
+
+
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=2,
+    train=dict(
+        type='BottomUpFishDataset',
+        ann_file=f'{data_root}/annotations/dlc_shuffle0_train.json',
+        img_prefix=f'{data_root}/images',
+        data_cfg=data_cfg,
+        pipeline=train_pipeline),
+    val=dict(
+        type='BottomUpFishDataset',
+        ann_file=f'{data_root}/annotations/dlc_shuffle0_val.json',
+        img_prefix=f'{data_root}/images',
+        data_cfg=data_cfg,
+        pipeline=val_pipeline),
+    test=dict(
+        type='BottomUpFishDataset',
+        ann_file=f'{data_root}/annotations/dlc_shuffle0_val.json',
+        img_prefix=f'{data_root}/images',
+        data_cfg=data_cfg,
+        pipeline=val_pipeline),
+)
